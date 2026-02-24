a63bbbfaac42bbb3addb5f933d48e5fa83f869a9
DolgovIA <divan6101@gmail.com>
2026-02-24 16:21:22 +0900

    Первый коммит В коммит эдите можно сделать В несколько строчек как сейчас

---
+++ gitworks.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+//1. Создаем репозиторий командой git init
+//2. Добавляем текущий файл в репозиторий командов git add gitworks.cpp
+//3. Фиксируем изменения в файле командой git commit "Описание изменения"
